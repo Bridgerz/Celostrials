0171df12b71a65e0c8cd701121183f2bc20114e1
bridgerz <bridgerzoske@gmail.com>
2022-02-07 09:29:29 -0800

    Update index.js

--- packages/rarity/config/index.js
+++ packages/rarity/config/index.js
@@ -1,7 +1,7 @@
 export const config = {
   env: process.env.ENVIRONEMNT,
   LOCAL_API_URL: "localhost:3000",
-  // API_URL: "odp-rarity.vercel.app",
+  API_URL: "celostrials.vercel.app",
   COLLECTION_NAME: "Celostrials",
   COLLECTION_TITLE: "Celostrials",
   COLLECTION_DESCRIPTION: "nfETs on the Celo blockchain.",
